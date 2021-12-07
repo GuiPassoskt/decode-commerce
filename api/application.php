63a4ec1c3cf5fa3c7e2d8898766e3c5d35db9306
Guilherme Moreira <guilherme.moreira@consultegeo.com.br>
2021-12-07 09:20:37 -0300

    simplify

--- api/application.php
+++ api/application.php
@@ -1,3 +1,22 @@
 <?php
 
-header('Location: ./public/');
+$client = $_POST['client_id'];
+$category = $_POST['category_id'];
+$file = $_FILES['file'];
+
+$imageFileType = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+
+if (isset($file)) {
+    $newName = uniqid('decode-commerce', true) . "." . $imageFileType;
+    $file->move(ROOTPATH."public/assets/$client/$category", $newName);
+    return json_encode([
+        'status' => 201,
+        'msg' => 'arquivo enviado com sucesso!',
+        'path' => base_url("assets/$client/$category/$newName")
+    ]);
+} else {
+    return json_encode([
+        'status' => 'failed', 
+        'msg' => 'erro ao tentar enviar arquivo'
+    ]);
+}
