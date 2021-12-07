49fb2cdd03f3fa2a097db34022997e9734b73465
Guilherme Moreira <guilherme.moreira@consultegeo.com.br>
2021-12-07 09:30:51 -0300

    fixed msg

--- api/application.php
+++ api/application.php
@@ -1,8 +1,31 @@
 <?php
 
-$client = $_POST['client_id'];
-$category = $_POST['category_id'];
-$file = $_FILES['file'];
+if (isset($_POST['client_id'])) {
+    $client = $_POST['client_id'];
+} else {
+    return json_encode([
+        'status' => 404,
+        'msg' => 'Client ID não informado!'
+    ]);
+}
+
+if (isset($_POST['category_id'])) {
+    $category = $_POST['category_id'];
+} else {
+    return json_encode([
+        'status' => 404,
+        'msg' => 'Categoria ID não informada!'
+    ]);
+}
+
+if (isset($_FILES['file'])) {
+    $file = $_FILES['file'];
+} else {
+    return json_encode([
+        'status' => 404,
+        'msg' => 'Arquivo não anexado!'
+    ]);
+}
 
 $imageFileType = strtolower(pathinfo($file, PATHINFO_EXTENSION));
 
